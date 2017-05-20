33b1671212825c303a836064163ae0d679c04bf1
Spinaker1 <klitschko456@gmail.com>
2017-05-20 18:27:17 +0200

    changes in GameView

--- src/mvc/GameView.java
+++ src/mvc/GameView.java
@@ -1,23 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package mvc;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import media.graphics.Board;
-import media.graphics.MainMenu;
+import media.graphics.*;
 
 public class GameView extends Application 
 {
     private MainMenu mainMenu;
     private Board board;
-    
+    OptionsPanel optionsPanel;
+    NetworkGUI networkGUI;
+
     @Override
     public void start(Stage primaryStage) 
     {
         mainMenu = new MainMenu(20,this);
         board = new Board(8,this);
+        optionsPanel = new OptionsPanel(mainMenu);
+        networkGUI  = new NetworkGUI(mainMenu);
         
-        Scene scene = new Scene(mainMenu, 700, 700);
+        Scene scene = new Scene(mainMenu, 700, 700, Color.WHITESMOKE);
  
         primaryStage.setTitle("Reversi");
         primaryStage.setScene(scene);
@@ -48,5 +57,16 @@ public class GameView extends Application
     public void setBoard(Board board) 
     {
         this.board = board;
+    }
+
+    public OptionsPanel getOptionsPanel() { return optionsPanel; }
+
+    public void setOptionPanel(OptionsPanel optionsPanel) { this.optionsPanel = optionsPanel; }
+
+    public NetworkGUI getNetworkGUI() { return networkGUI; }
+
+    public void setNetworkGUI(NetworkGUI networkGUI)
+    {
+        this.networkGUI = networkGUI;
     }
 }
